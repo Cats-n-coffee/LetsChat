c8d49bfd6cb1e9b046af30b819763530bd8d4a1c
Cats-n-coffee <lucie.macaigne@gmail.com>
2024-05-15 19:33:31 -0400

    server with threads, client take user input

--- chatAppClient/src/main.cpp
+++ chatAppClient/src/main.cpp
@@ -25,7 +25,9 @@ int main()
     serverAddr.sin_port = htons(5051); //  Server Port
     inet_aton("127.0.0.1", &serverAddr.sin_addr); // ipv4 address
 
-    if (connect(tcpSocket, (struct sockaddr *) &serverAddr, sizeof(serverAddr)) == -1) {
+    int connection = connect(tcpSocket, (struct sockaddr *) &serverAddr, sizeof(serverAddr));
+
+    if (connection == -1) {
         std::cout << "error connection " << strerror(errno) << strerror(errno) << std::endl;
         return -1;
     }
@@ -33,20 +35,31 @@ int main()
     std::cout << "after connection" << std::endl;
 
     // ============= Send message
-    std::string message = "This is a message"; // should prob check the length
-    char buf[100];
-    strncpy(buf, message.c_str(), sizeof(buf));
-    buf[sizeof(buf) - 1] = 0;
+    while (connection != -1) {
+        std::string userMessage = "";
 
-    if (send(tcpSocket, buf, sizeof(buf), 0) == -1) {
-        std::cout << "error send" << strerror(errno) << std::endl;
-        return -1;
+        std::cout << "Enter your message: " << std::endl;
+        std::getline(std::cin, userMessage);
+        std::cout << "entrered " << userMessage << std::endl;
+
+        char buf[100];
+        strncpy(buf, userMessage.c_str(), sizeof(buf));
+        buf[sizeof(buf) - 1] = 0;
+
+        if (send(tcpSocket, buf, sizeof(buf), 0) == -1) {
+            std::cout << "error send" << strerror(errno) << std::endl;
+            return -1;
+        }
+        std::cout << "after send" << std::endl;
     }
-    std::cout << "after send" << std::endl;
+    
 
     // ============== Close Socket
     if (close(tcpSocket) == -1) {
         std::cout << "error closing" << strerror(errno) << std::endl;
         return -1;
     }
+
+    std::cout << "Client closed" << std::endl;
+    return 0;
 }
--- chatAppServer/src/main.cpp
+++ chatAppServer/src/main.cpp
@@ -1,5 +1,7 @@
 #include <iostream>
 #include <string>
+#include <vector>
+#include <thread>
 #include <string.h>
 #include <errno.h>
 #include <netinet/in.h>
@@ -7,8 +9,59 @@
 #include <arpa/inet.h>
 #include <sys/socket.h>
 
+// =========== All connections
+std::vector<int> allClients = {};
+
+// =========== Threads handlers
+
+// Create thread to loop over connected clients
+void handleMessages(std::vector<int> connections)
+{
+    std::cout << "======= inside message thread" << std::endl;
+    // use poll() here
+    // for (int connectedSocket : connections) {
+    //     std::cout << "======= current " << connectedSocket << std::endl;
+
+    //     // ============= Read message from client
+    //     char messageBuf[100];
+    //     ssize_t receivedMessage = recv(connectedSocket, messageBuf, sizeof(messageBuf), 0);
+
+    //     if (receivedMessage == -1) {
+    //         std::cout << "error recv " << strerror(errno) << std::endl;
+    //         // return -1;
+    //     }
+
+    //     std::cout << "received message length " << receivedMessage << " Message: " << messageBuf << std::endl;
+    // }
+    std::cout << "======= end of message thread" << std::endl;
+}
+
+// Accept client connections
+void handleConnections(int tcpSocket)
+{
+    std::cout << "======= inside  accept thread" << std::endl;
+    while (1) {
+        struct sockaddr_in connectionSocketAddress;
+        socklen_t connectionSocketAddressSize = sizeof(connectionSocketAddress);
+        int connectedSocket = accept(tcpSocket, (struct sockaddr *) &connectionSocketAddress, &connectionSocketAddressSize);
+        
+        std::cout << "after accept" << connectedSocket << std::endl;
+
+        if (connectedSocket == -1) {
+            std::cout << "error accept" << strerror(errno) << std::endl;
+            // return -1;
+        }
+
+        allClients.push_back(connectedSocket);
+    }
+    std::cout << "======= end of accept thread" << std::endl;
+}
+
+// =========== Server
 int main()
 {
+    std::vector<std::thread> threads = {};
+
     // =========== Create a socket
     int tcpSocket = socket(AF_INET, SOCK_STREAM, 0);
 
@@ -39,29 +92,13 @@ int main()
     }
     std::cout << "after listen" << std::endl;
 
-    // ============= Accept client connections
-    while (1) {
-        struct sockaddr_in connectionSocketAddress;
-        socklen_t connectionSocketAddressSize = sizeof(connectionSocketAddress);
-        int connectedSocket = accept(tcpSocket, (struct sockaddr *) &connectionSocketAddress, &connectionSocketAddressSize);
-        
-        std::cout << "after accept" << connectedSocket << std::endl;
+    // ============  Create threads for connections and messages
 
-        if (connectedSocket == -1) {
-            std::cout << "error accept" << strerror(errno) << std::endl;
-            return -1;
-        }
+    threads.push_back(std::thread(handleConnections, tcpSocket));
+    threads.push_back(std::thread(handleMessages, allClients));
 
-        // ============= Read message from client
-        char messageBuf[100];
-        ssize_t receivedMessage = recv(connectedSocket, messageBuf, sizeof(messageBuf), 0);
-
-        if (receivedMessage == -1) {
-            std::cout << "error recv" << strerror(errno) << std::endl;
-            return -1;
-        }
-
-        std::cout << "received message length " << receivedMessage << " Message: " << messageBuf << std::endl;
+    for (int i = 0; i < threads.size(); i++) {
+        threads[i].join();
     }
 
     // ============== Close socket
@@ -69,4 +106,7 @@ int main()
         std::cout << "error closing" << strerror(errno) << std::endl;
         return -1;
     }
+
+    std::cout << "Server closed" << std::endl;
+    return 0;
 }
